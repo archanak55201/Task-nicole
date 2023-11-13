3011cebb5a9a6c4366601d8c1ffc74694e5a7b88
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-11-13 16:05:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Question from './components/question'
+import Question from './components/Question'
 import {Routes,Route} from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0)
